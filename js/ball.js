c4850b11f7c4297b514434f0de4d0b8ef3cdde0b
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-03-21 20:50:54 -0400

    Create Ball Object

---
+++ js/ball.js
@@ -0,0 +1,10 @@
+const RenderBall = require('./render_ball');
+
+class Ball {
+  constructor(ctx) {
+    this.radius = 10;
+    this.xCoord = 50;
+    this.yCoord = 100;
+    this.vel = 2;
+  }
+}
--- js/entry.js
+++ js/entry.js
@@ -1,15 +1,14 @@
 const Paddle = require("./paddle");
-
+const Ball = require("./ball");
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvasEl = document.getElementById("game-canvas");
   canvasEl.width = 800;
   canvasEl.height = 500;
-
-
   const ctx = canvasEl.getContext("2d");
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+  ball = new Ball(ctx);
+  ball.render();
   window.ctx = ctx
-
 });
