2639fd927e76564a98f406860883cb995ceb6334
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-03-22 16:52:34 -0400

    Paddle cant move past walls

--- js/ball.js
+++ js/ball.js
@@ -15,18 +15,15 @@ class Ball extends MovingObject {
   }
 
   move() {
-
     if(this.pos[0] + this.vel[0] > 800 - this.radius || this.pos[0] + this.vel[0] < this.radius) {
       this.vel[0] *= -1;
     }
-
     if(this.pos[1] + this.vel[1] > 500 - this.radius || this.pos[1] + this.vel[1] < this.radius) {
       this.vel[1] *= -1;
     }
     this.pos[0] += this.vel[0];
     this.pos[1] += this.vel[1];
   }
-
 }
 
 export default Ball;
--- js/paddle.js
+++ js/paddle.js
@@ -16,7 +16,11 @@ class Paddle extends MovingObject {
   move(timeDelta){
     const velScale = timeDelta / NORMAL_FRAME_TIME_DELTA;
     let offsetX = this.vel[0] * velScale;
-    this.pos = [this.pos[0] + offsetX, this.pos[1]];
+    if(this.pos[0] + this.vel[0] >= 800 - this.width || this.pos[0] + this.vel[0] < 0) {
+      this.vel[0] = 0;
+    } else {
+      this.pos = [this.pos[0] + offsetX, this.pos[1]];
+    }
   }
 
 }
