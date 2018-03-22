442ab277b4970c6183c0032e01a19f4924e795d9
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-03-21 20:22:40 -0400

    set up files

--- js/entry.js
+++ js/entry.js
@@ -1,1 +1,15 @@
-console.log("Webpack is not working!")
+const Paddle = require("./paddle");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvasEl = document.getElementById("game-canvas");
+  canvasEl.width = 800;
+  canvasEl.height = 500;
+
+
+  const ctx = canvasEl.getContext("2d");
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+  window.ctx = ctx
+
+});
