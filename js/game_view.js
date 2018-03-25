9125975556382c72326bf5569a82fe8010df5014
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-03-24 21:16:37 -0400

    adjust speed of ball depending on level

--- js/game_view.js
+++ js/game_view.js
@@ -32,7 +32,9 @@ class GameView {
    } else if (event.keyCode === 39) {
      this.paddle.vel[0] += 7;
    } else if (this.ball.vel[0] === 0 && this.ball.vel[0] === 0 && event.keyCode === 32) {
-     this.ball.vel = [5,-5];
+     const x = 5 * this.game.level;
+     const y = 5 * this.game.level
+     this.ball.vel = [x,y];
      this.game.reset = 0;
    } else if (event.keyCode === 83) {
      this.game.level = 1;
@@ -45,7 +47,9 @@ class GameView {
    } else if(event.keyCode === 37 || event.keyCode === 39){
      this.paddle.vel[0] = 0;
    } else if (this.ball.vel[0] === 0 && this.ball.vel[0] === 0 && event.keyCode === 32) {
-     this.ball.vel = [5,-5];
+     const x = 5 * this.game.level;
+     const y = 5 * this.game.level
+     this.ball.vel = [x,y];
      this.game.reset = 0;
    } else if (event.keyCode === 83) {
      this.game.level = 1;
